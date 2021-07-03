497c1bf9d374a205f6f213af5e72ec665955c573
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-07-03 18:11:38 +0100

    test predictor with the static data that I've saved

--- predictor.js
+++ predictor.js
@@ -1,5 +1,6 @@
 const brain = require('brain.js')
 const scaler = require('minmaxscaler')
+const trainingData = require('./stockData.json').map(obj => obj["close"])
 const net = new brain.recurrent.LSTMTimeStep({
     inputSize: 1,
     hiddenLayers: [3, 3],
@@ -9,18 +10,22 @@ const net = new brain.recurrent.LSTMTimeStep({
 function predictor(data) {
     const trainingData = format(scaler.fit_transform(data))
 
-    net.train(trainingData, {
+    const stats = net.train(trainingData, {
         learningRate: 0.005,
         errorThresh: 0.02,
     })
+    console.log(stats)
 
-    return net.forecast(trainingData[trainingData.length - 1], 10)
+    console.log(scaler.inverse_transform(trainingData[trainingData.length - 1]))
+    return scaler.inverse_transform(net.forecast(trainingData[trainingData.length - 2], 5))
 }
 
 function format(arr) {
     const toReturn = []
-    for(let i= 0; i<scaledPrices.length; i+=5) {
-        toReturn.push(scaledPrices.slice(i, i+5))
+    for(let i= 0; i<arr.length; i+=5) {
+        toReturn.push(arr.slice(i, i+5))
     }
     return toReturn
 }
+
+console.log(predictor(trainingData))
